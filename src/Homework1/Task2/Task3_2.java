2d7184a3ce4efe020a27d1397832c39696b9f21d
olnikiforov <nikiforovsh@gmail.com>
2023-02-09 13:15:09 +0200

    [HW1] - DONE!

--- src/Homework1/Task2/Task3_2.java
+++ src/Homework1/Task2/Task3_2.java
@@ -1,6 +1,6 @@
 package Homework1.Task2;
 
-public class task3_2 {
+public class Task3_2 {
     public static void main(String[] args) {
         ConcreteCreator1 creator1 = new ConcreteCreator1();
         ConcreteCreator2 creator2 = new ConcreteCreator2();
